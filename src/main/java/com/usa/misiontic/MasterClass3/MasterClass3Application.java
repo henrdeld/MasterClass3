be29c61c8067276b321a5deb01c25933a5a22e16
henrdeld <henry8222@gmail.com>
2022-10-17 09:49:23 -0500

    Reto 5 *****- Reto 5 - prueba de subida

--- src/main/java/com/usa/misiontic/MasterClass3/MasterClass3Application.java
+++ src/main/java/com/usa/misiontic/MasterClass3/MasterClass3Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MasterClass3Application {
+public class MasterClass3Application  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MasterClass3Application.class, args);
